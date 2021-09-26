70ef055272a3feac1ef094f3ef22be5af419df6c
Andrey O <ozzy.ext@gmail.com>
2021-09-26 03:51:28 +0300

    Introduce AppModel ans sections

---
+++ src/MyLab.BlazorClient/Services/ServiceCollectionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyLab.BlazorClient.Model;
+
+namespace MyLab.BlazorClient.Services
+{
+    /// <summary>
+    /// Extensions for <see cref="IServiceCollection"/>
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMyLabApp(this IServiceCollection services, AppModel appModel)
+        {
+            return services.AddSingleton(appModel);
+        }
+    }
+}
